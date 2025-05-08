8dec773922f8b4f08067f505956721d5cf38773b
Mohamed Said Lehbib <lehbibm@pcserveur.ensimag.fr>
2025-05-08 13:19:20 +0200

    correction du lec_ppm

--- src/lec_ppm.c
+++ src/lec_ppm.c
@@ -6,45 +6,63 @@
 void transf_ppm(uint32_t tab[8][8], const char* nom){
     
 
-    FILE *fichier = fopen(nom, "w");
+    FILE *fichier = fopen(nom, "wb");
     fprintf(fichier, "P6\n");
     fprintf(fichier, "8 8\n");
     fprintf(fichier, "255\n");
-    
+    uint8_t  b, g, r;
     for (uint32_t i =0; i < 8; i++){
         for (uint32_t j =0; j < 8; j++){
-            uint8_t  b = (tab[i][j]);
-            uint8_t  g = (tab[i][j] >>8);
-            uint8_t  r = (tab[i][j] >>16);
+            b = (tab[i][j])  & 0xFF;
+            g = (tab[i][j] >>8) & 0xFF;
+            r = (tab[i][j] >>16) & 0xFF;
             fwrite(&r, sizeof(uint8_t), 1, fichier);
             fwrite(&g, sizeof(uint8_t), 1, fichier);
             fwrite(&b, sizeof(uint8_t), 1, fichier);
-        } 
-        fprintf(fichier,"\n");
+        }
     }
-
     fclose(fichier);
 }
-
-
-
 
 
 void transf_pgm(uint32_t tab[8][8], const char* nom){
     
 
-    FILE *fichier = fopen(nom, "w");
-    fprintf(fichier, "P6\n");
+    FILE *fichier = fopen(nom, "wb");
+    fprintf(fichier, "P5\n");
     fprintf(fichier, "8 8\n");
     fprintf(fichier, "255\n");
-    
+    uint8_t b;
     for (uint32_t i =0; i < 8; i++){
         for (uint32_t j =0; j < 8; j++){
-            uint8_t  b = (tab[i][j]);
+            if (tab[i][j] > 255){
+                b = 255;
+            }
+            else
+                b = (tab[i][j]);
             fwrite(&b, sizeof(uint8_t), 1, fichier);
-        } 
-        fprintf(fichier,"\n");
+        }
     }
 
     fclose(fichier);
 }
+
+// teste //
+
+
+// int main() {
+//     uint32_t image[8][8];
+
+//     for (int i = 0; i < 8; i++) {
+//         for (int j = 0; j < 8; j++) {
+//             uint8_t r = i * 32;   
+//             uint8_t g = j * 32;   
+//             uint8_t b = 0;        
+//             image[i][j] = (r << 16) | (g << 8) | b;
+//         }
+//     }
+
+//     transf_ppm(image, "test.ppm");
+//     printf("Fichier test.ppm généré.\n");
+//     return 0;
+// }
