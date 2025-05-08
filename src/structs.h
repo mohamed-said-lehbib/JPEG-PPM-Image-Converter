3d3d5976ec101e07afa9a499ca9a7a4595cdd173
abdellah <abdellah.bensalem@grenoble-inp.org>
2025-05-09 00:00:03 +0200

    structures en 1 seule fish

---
+++ src/structs.h
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+//valeurs de la section sos
+typedef struct{
+    uint8_t i_c;
+    uint8_t i_ac;
+    uint8_t i_dc;
+
+} SOS_val;
+//tables de quantification
+typedef struct {
+    uint8_t prec;//pour se souvenir de typpe de table
+    uint8_t i_q;
+    void *data;
+} quantification_table;
+//longuers et symboles de huffmann
+typedef struct{
+    uint8_t *lengths;
+    uint8_t* symboles;
+} huff_tbl;
+//valeurs de l'entête SOF,informations des composantes
+typedef struct {
+    uint8_t i_c;
+    uint8_t h_i;
+    uint8_t v_i;
+    uint8_t i_q;//table de quantification
+} infos_comp;
