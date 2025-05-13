41c81118aa05943ab5a41f2dfdf2a2b60dc995bf
Mohamed Said Lehbib <lehbibm@pcserveur.ensimag.fr>
2025-05-13 21:57:37 +0200

    rf

--- src/YCbCr2RGB.c
+++ src/YCbCr2RGB.c
@@ -8,8 +8,8 @@ uint32_t** YCbCr2RGB(uint8_t **Y, uint8_t **Cb, uint8_t **Cr,uint8_t hauteur,uin
     for (int i =0; i < largeur; i++){
         pix[i]= malloc(hauteur*sizeof(uint32_t));
     }
-    for (uint16_t i  = 0; i <= sizeof(Y)/8; i++){ 
-        for (uint16_t j  = 0; j <= sizeof(Y[0]); j++){   
+    for (uint16_t i  = 0; i <= hauteur; i++){ 
+        for (uint16_t j  = 0; j <= largeur; j++){   
             uint8_t R = Y[i][j] -0.0009267 * (Cb[i][j]-128)+1.4016868*(Cr[i][j]-128);
             uint8_t G = Y[i][j] -0.3436954 * (Cb[i][j]-128)-0.7141690*(Cr[i][j]-128);
             uint8_t B = Y[i][j] +1.7721604 * (Cb[i][j]-128)+0.0009902*(Cr[i][j]-128);
