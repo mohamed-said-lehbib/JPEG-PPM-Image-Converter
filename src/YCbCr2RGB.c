19495ffc669ed851c88082b04c4dfa6afa58be0f
Mohamed Said Lehbib <lehbibm@ensipc290.ensimag.fr>
2025-05-13 12:46:46 +0200

    rgb

--- src/YCbCr2RGB.c
+++ src/YCbCr2RGB.c
@@ -3,13 +3,16 @@
 #include <stdint.h>
 
 
-uint32_t f(uint32_t Y[8][8], uint32_t Cb[8][8], uint32_t Cr[8][8]){
-    uint32_t pix [8][8];
-    for (uint16_t i  = 0; i <= sizeof(Y[0]); i++){ 
+uint32_t** f(uint8_t *Y, uint8_t *Cb, uint8_t *Cr,uint8_t hauteur,uint8_t largeur){
+    uint32_t **pix = malloc(largeur*sizeof(uint32_t*));
+    for (int i =0; i < largeur; i++){
+        pix[i]= malloc(hauteur*sizeof(uint32_t));
+    }
+    for (uint16_t i  = 0; i <= sizeof(Y)/8; i++){ 
         for (uint16_t j  = 0; j <= sizeof(Y[0]); j++){   
-            uint8_t R = Y[i][j] -0.0009267*(Cb[i][j]-128)+1.4016868*(Cr[i][j]-128);
-            uint8_t G = Y[i][j] -0.3436954*(Cb[i][j]-128)-0.7141690*(Cr[i][j]-128);
-            uint8_t B = Y[i][j] +1.7721604*(Cb[i][j]-128)+0.0009902*(Cr[i][j]-128);
+            uint8_t R = Y[i*8+j] -0.0009267 * (Cb[i*8+j]-128)+1.4016868*(Cr[i*8+j]-128);
+            uint8_t G = Y[i*8+j] -0.3436954 * (Cb[i*8+j]-128)-0.7141690*(Cr[i*8+j]-128);
+            uint8_t B = Y[i*8+j] +1.7721604 * (Cb[i*8+j]-128)+0.0009902*(Cr[i*8+j]-128);
             pix[i][j] = (R << 16) | (G << 8) | B;
         }
     }
