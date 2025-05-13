258bc0e80eb5163e2afd3826fed4e38738eaf4a3
Mohamed Said Lehbib <lehbibm@ensipc290.ensimag.fr>
2025-05-13 14:12:17 +0200

    zigzag

---
+++ src/izigzag.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int16_t **zigzag_inv(int *quan_vect){
+    uint8_t poin = 0; //indice dans le vectteur zigzagué
+    
+    uint8_t i =0;
+    uint8_t j =0;//indices de case
+
+    
+        int16_t **table = malloc(8*sizeof(int16_t*));
+        for (int i=0; i<8; i++){
+            table[i] = malloc(8*sizeof(uint8_t));
+        }
+        int *vect8 = quan_vect;
+        while( poin < 64){
+            
+            table[i][j] = vect8[poin];
+            if( (i + j )% 2 ==0){//monter
+                if( i>0){
+                    if(j<7){
+                        j++;
+                        i--;
+                    }
+                    else{
+                        i++;
+                    }
+                   
+            }
+                else{
+                    j++;//ici il n'ya pas le cas où les deux s'arrivent
+                }
+            }
+            else{//descente
+                if(i<7){
+                    if(j>0){
+                        i++;
+                        j--;
+                    }
+                    else{
+                        i++;// cas de (1,0)
+                    }
+                }
+                else{
+                    j++;//en tout cas on est en bas
+                }
+            }
+            poin++;
+
+
+        }
+        return table;
+    }
