d335e780c28dd0e11d3df2469b1eee7595a6beec
lehbibm <mohamed-said.lehbib@grenoble-inp.fr>
2025-05-17 14:26:40 +0200

    pgm

--- src/ecriture_ppm.c
+++ src/ecriture_ppm.c
@@ -60,62 +60,84 @@ void transf_ppm(uint32_t** t,const char* nom, uint16_t largeur, uint16_t hauteur
 
 
 
-void transf_pgm_col(uint32_t*** t, const char* nom, uint16_t largeur, uint16_t hauteur) {
-     uint16_t nb_mcux = (largeur+7)/8;
-    uint16_t nb_mcuy = (hauteur+7)/8;
+// void transf_pgm_col(uint32_t*** t, const char* nom, uint16_t largeur, uint16_t hauteur) {
+//      uint16_t nb_mcux = (largeur+7)/8;
+//     uint16_t nb_mcuy = (hauteur+7)/8;
 
-    FILE *fichier = fopen(nom, "wb");
+//     FILE *fichier = fopen(nom, "wb");
   
-    fprintf(fichier, "P6\n");
-    fprintf(fichier, "%d %d\n", largeur, hauteur);
-    fprintf(fichier, "255\n");
-    uint8_t b;
-    for (uint16_t mcu_y = 0; mcu_y < nb_mcuy; mcu_y++) {
-        for (uint8_t i = 0; i < 8; i++) {
-             if( mcu_y*8 +i >= hauteur){break;}
-            for (uint16_t mcu_x = 0; mcu_x < nb_mcux; mcu_x++) {
-                uint16_t k = mcu_y * nb_mcux + mcu_x; 
-                for (uint8_t j = 0; j < 8; j++) {   
-                    if( mcu_x*8 +j >= largeur){break;}
-                    if (t[k][i][j] > 255){
-                        b = 255;
-                    }
-                    else {
-                        b= t[k][i][j];
-                    }
-                    fwrite(&b, sizeof(uint8_t), 1, fichier);
-                }
-            }
-        }
-    }
-    fclose(fichier);
-}
-void transf_pgm(uint8_t**** t, const char* nom, uint16_t largeur, uint16_t hauteur) {
-       uint16_t nb_mcux = (largeur+7)/8;
-    uint16_t nb_mcuy = (hauteur+7)/8;
+//     fprintf(fichier, "P6\n");
+//     fprintf(fichier, "%d %d\n", largeur, hauteur);
+//     fprintf(fichier, "255\n");
+//     uint8_t b;
+//     for (uint16_t mcu_y = 0; mcu_y < nb_mcuy; mcu_y++) {
+//         for (uint8_t i = 0; i < 8; i++) {
+//              if( mcu_y*8 +i >= hauteur){break;}
+//             for (uint16_t mcu_x = 0; mcu_x < nb_mcux; mcu_x++) {
+//                 uint16_t k = mcu_y * nb_mcux + mcu_x; 
+//                 for (uint8_t j = 0; j < 8; j++) {   
+//                     if( mcu_x*8 +j >= largeur){break;}
+//                     if (t[k][i][j] > 255){
+//                         b = 255;
+//                     }
+//                     else {
+//                         b= t[k][i][j];
+//                     }
+//                     fwrite(&b, sizeof(uint8_t), 1, fichier);
+//                 }
+//             }
+//         }
+//     }
+//     fclose(fichier);
+// }
+// void transf_pgm(uint8_t**** t, const char* nom, uint16_t largeur, uint16_t hauteur) {
+//        uint16_t nb_mcux = (largeur+7)/8;
+//     uint16_t nb_mcuy = (hauteur+7)/8;
 
-    FILE *fichier = fopen(nom, "wb");
+//     FILE *fichier = fopen(nom, "wb");
  
+//     fprintf(fichier, "P5\n");
+//     fprintf(fichier, "%d %d\n", largeur, hauteur);
+//     fprintf(fichier, "255\n");
+//     uint8_t b;
+//     for (uint16_t mcu_y = 0; mcu_y < nb_mcuy; mcu_y++) {
+//         for (uint8_t i = 0; i < 8; i++) {
+//              if( mcu_y*8 +i >= hauteur){break;}
+//             for (uint16_t mcu_x = 0; mcu_x < nb_mcux; mcu_x++) {
+//                 uint16_t k = mcu_y * nb_mcux + mcu_x; 
+//                 for (uint8_t j = 0; j < 8; j++) {  
+//                       if( mcu_x*8 +j >= largeur){break;}
+//                     if (t[k][0][i][j] > 255){
+//                         b = 255;
+//                     }
+//                     else {
+//                         b= t[k][0][i][j];
+//                     }
+//                     fwrite(&b, sizeof(uint8_t), 1, fichier);
+//                 }
+//             }
+//         }
+//     }
+//     fclose(fichier);
+// }
+
+
+void transf_pgm(uint32_t** t,const char* nom, uint16_t largeur, uint16_t hauteur){
+    FILE *fichier = fopen(nom, "wb");
     fprintf(fichier, "P5\n");
     fprintf(fichier, "%d %d\n", largeur, hauteur);
     fprintf(fichier, "255\n");
     uint8_t b;
-    for (uint16_t mcu_y = 0; mcu_y < nb_mcuy; mcu_y++) {
-        for (uint8_t i = 0; i < 8; i++) {
-             if( mcu_y*8 +i >= hauteur){break;}
-            for (uint16_t mcu_x = 0; mcu_x < nb_mcux; mcu_x++) {
-                uint16_t k = mcu_y * nb_mcux + mcu_x; 
-                for (uint8_t j = 0; j < 8; j++) {  
-                      if( mcu_x*8 +j >= largeur){break;}
-                    if (t[k][0][i][j] > 255){
-                        b = 255;
-                    }
-                    else {
-                        b= t[k][0][i][j];
-                    }
-                    fwrite(&b, sizeof(uint8_t), 1, fichier);
-                }
+    for (uint32_t i =0;i<hauteur;i++){
+        for (uint32_t j = 0; j < largeur; j++) {  
+            // if( i*8 +j >= largeur){break;}
+            if (t[i][j] > 255){
+                b = 255;
             }
+            else {
+                b= t[i][j];
+            }
+            fwrite(&b, sizeof(uint8_t), 1, fichier);
         }
     }
     fclose(fichier);
