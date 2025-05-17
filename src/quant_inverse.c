affa8ae20916b94288b5ba3846bd512f965969c1
lehbibm <mohamed-said.lehbib@grenoble-inp.fr>
2025-05-17 13:12:17 +0200

    ff

--- src/quant_inverse.c
+++ src/quant_inverse.c
@@ -8,7 +8,7 @@ void quant_inverse(Bloc *bloc, quantification_table *qt) {
     uint8_t *quant_table = (uint8_t *)qt->data;
     for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                bloc->data[i*8+j] = (bloc->data[i+j] * quant_table[i * 8 + j]);
+                bloc->data[i*8+j] = (bloc->data[i*8+j] * quant_table[i * 8 + j]);
             }
             
         }
