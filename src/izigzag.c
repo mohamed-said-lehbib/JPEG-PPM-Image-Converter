f8d5954453f65273e958791eec339a3c1ac89017
abdellah <abdellah.bensalem@grenoble-inp.org>
2025-05-19 22:55:45 +0200

    zgzg_cmnt

--- src/izigzag.c
+++ src/izigzag.c
@@ -2,55 +2,56 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int16_t **zigzag_inv(int16_t *quan_vect){
+int16_t **zigzag_inv(int16_t *vect_in){//effectuer le zigzag inverse sur le vecteur input et retourne un int16
     uint8_t poin = 0; //indice dans le vectteur zigzagué
     
     uint8_t i =0;
     uint8_t j =0;//indices de case
 
-    
-        int16_t **table = malloc(8*sizeof(int16_t*));
-        for (int i=0; i<8; i++){
-            table[i] = malloc(8*sizeof(int16_t));
-        }
-        int16_t *vect8 = quan_vect;
-        while( poin < 64){
-            
-            table[i][j] = vect8[poin];
-            if( (i + j )% 2 ==0){//monter
-                if( i>0){
-                    if(j<7){
-                        j++;
-                        i--;
-                    }
-                    else{
-                        i++;
-                    }
-                   
-            }
-                else{
-                    j++;//ici il n'ya pas le cas où les deux s'arrivent
+    //allouer de la mémoire pour l'output
+    int16_t **table = malloc(8*sizeof(int16_t*));
+    for (int i=0; i<8; i++){
+        table[i] = malloc(8*sizeof(int16_t));
+    }
+   
+    while( poin < 64){
+       
+        table[i][j] = vect_in[poin];
+        if( (i + j )% 2 ==0){//caractérise l montée
+            if( i>0){
+                if(j<7){//cas normal
+                    j++;
+                    i--;
                 }
+                else{//on est au bordure droit,donc on descent
+                    i++;
+                }
+                
+        }
+            else{
+                j++;//on en haut,donc on s'oriente à droite
             }
-            else{//descente
-                if(i<7){
-                    if(j>0){
-                        i++;
-                        j--;
-                    }
-                    else{
-                        i++;// cas de (1,0)
-                    }
+        }
+        else{//descente
+            if(i<7){
+                if(j>0){//cas normal
+                    i++;
+                    j--;
                 }
                 else{
-                    j++;//en tout cas on est en bas
+                    i++;// cas de (1,0),on est au bordure gauche donc on descent
                 }
             }
-            poin++;
-
-
+            else{
+                j++;//on est en bas donc on s'oriente à droite
+            }
         }
-        
-        return table;
+        poin++;
+
 
     }
+    
+    return table;
+    //quan vect déja libérée dans le main
+
+}
